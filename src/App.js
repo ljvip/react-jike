01ac5e351eb87fcea2d83c17d8d05dc795c6eb94
ljvip <ljvip3m@gmail.com>
2023-10-28 16:40:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>这是一个新项目</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd'
+
+const Login = ()=> {
+    return(
+        <div>
+            我是Login
+            <Button type='primary'>登录</Button>
+        </div>
+    )
+}
+export default Login
